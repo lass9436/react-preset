87bcdf0a3723cfdea440f13a743706e6b0bfe0f2
lass9436 <lass9436@gmail.com>
2022-10-13 12:18:07 +0900

    bolierplate 작성

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>hello world</p>
+		</div>
+	);
+}
+
+export default App;
